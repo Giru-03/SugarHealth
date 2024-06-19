cf13e57992c1f822e6e7130166072435c5453cbd
Girendra Singh <126884309+Giru-03@users.noreply.github.com>
2024-06-19 14:15:41 +0530

    Update urls.py

--- Predictor/urls.py
+++ Predictor/urls.py
@@ -6,4 +6,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
     path('accounts/',include('allauth.urls')),
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
