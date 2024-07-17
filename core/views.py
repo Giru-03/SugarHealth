0b5633a5674097dd8d273dbd3d245b3fae2b932a
Girendra Singh <126884309+Giru-03@users.noreply.github.com>
2024-07-17 09:16:25 +0000

    new

--- core/views.py
+++ core/views.py
@@ -7,11 +7,7 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
 from django.urls import reverse
 import numpy as np
-import pandas as pd
-from sklearn.preprocessing import StandardScaler
-from sklearn.model_selection import train_test_split
-from sklearn import svm
-from sklearn.metrics import accuracy_score
+import pickle
 
 def index(request):
     return render(request, 'index.html')
@@ -107,34 +103,11 @@ def predict(request):
         dia = float(request.POST['dia'])
         age = request.POST['age']
 
-        diabetes_dataset=pd.read_csv("./diabetes.csv")
-
-        diabetes_dataset.shape
-
-        diabetes_dataset['Outcome'].value_counts()
-
-        X=diabetes_dataset.drop(columns='Outcome', axis=1)
-        Y=diabetes_dataset['Outcome']
-
-        scaler=StandardScaler()
-        scaler.fit(X)
-
-        standardized_data=scaler.transform(X)
-
-        X=standardized_data
-        Y=diabetes_dataset['Outcome']
-
-        X_train,X_test,Y_train,Y_test=train_test_split(X,Y,test_size=0.2,stratify=Y,random_state=2)
-
-        classifier=svm.SVC(kernel='linear')
-
-        classifier.fit(X_train,Y_train)
-
-        X_train_prediction=classifier.predict(X_train)
-        training_data_accuracy=accuracy_score(X_train_prediction,Y_train)
-
-        X_test_prediction=classifier.predict(X_test)
-        test_data_accuracy=accuracy_score(X_test_prediction,Y_test)
+        with open('classifier.pkl','rb') as f:
+            classifier = pickle.load(f)
+        
+        with open('scaler.pkl','rb') as f:
+            scaler = pickle.load(f)
 
         input_data=(pregnancies, glucose, blood, skin, insulin, bmi, dia, age)
         input_data_as_nuumpy_array=np.asarray(input_data)
