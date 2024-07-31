3d9ab14ad51d4029b35b3137dac6930d89057a45
Girendra Singh <126884309+Giru-03@users.noreply.github.com>
2024-07-31 05:27:05 +0000

    new

--- Predictor/settings.py
+++ Predictor/settings.py
@@ -85,8 +85,12 @@ WSGI_APPLICATION = 'Predictor.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'UgNpdrjqGsrXaGZzoBbfeNtgbXHTJrtY',
+        'HOST': 'railway.app',
+        'PORT': '5432',
     }
 }
 
